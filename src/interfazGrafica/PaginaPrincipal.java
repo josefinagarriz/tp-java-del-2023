c24a15b76db2731852c3a222f1dfce22660d8d05
Josefina <josefinagarriz@yahoo.com.ar>
2025-03-02 12:22:42 -0300

    comienzo de interfaz grafica

--- src/interfazGrafica/PaginaPrincipal.java
+++ src/interfazGrafica/PaginaPrincipal.java
@@ -1,4 +1,15 @@
 package interfazGrafica;
 
-public class PaginaPrincipal {
+import logica.clases.Perfil;
+import excepciones.*;
+import logica.Reportes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PaginaPrincipal extends JFrame{
+
+    private Perfil perfil;
 }
